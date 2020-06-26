aec286a188e1a4504c2f966a961cfc78dd35dc87
kaweesa01 <42151017+kaweesa01@users.noreply.github.com>
2020-06-26 21:00:34 +0300

    Add files via upload

---
+++ precache-manifest.0631446180b4eed152dc03671944ccfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c976a48a36bb8e3ef66b1afc35513792",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f44d2a73dacb7df209",
+    "url": "/static/css/main.9c06f5ec.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f44d2a73dacb7df209",
+    "url": "/static/js/main.9e4a83c2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
